b863396aa305d91c76999bbe7b965afa8267437d
Lukas Oppermann <oppermann.lukas@googlemail.com>
2012-03-16 17:03:50 +1100

    working on basic design

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+	<meta name="author" content="Lukas Oppermann - veare.net" />
+	<meta name="description" content="Lukas Oppermann is a freelance interface & interaction designer who enjoys creating fast, usable and gorgeous interfaces." />
+	<meta name="robots" content="index,follow" />
+	<meta name="language" content="en" />
+	<link rel="favicon" href="./media/layout/favicon.ico" />
+	<link rel="stylesheet" type="text/css" href="./libs/css/screen.css" media="screen" />
+	<link rel="stylesheet" type="text/css" href="./libs/css/print.css" media="print" />
+	<title>Lukas Oppermann - interface & interaction designer | lukasoppermann.com</title>
+</head>
+<body>
+	<div id="about" class="column-right">
+		<h1>Lukas Oppermann</h1>
+		<p>I am an interface & information designer who enjoys devoting his life to plan, design & scrutinize fancy & usable interfaces. I also have a nack for typography.</p>
+	</div>
+	<div id="lukasoppermann_picture" class="column-left">
+		<img src="./media/lukasoppermann.jpg" alt="Interface Designer - Lukas Oppermann">
+	</div>
+	<div id="social_media" class="column-left">
+		
+	</div>
+	<div id="twitter" class="column-right">
+		<div id="tweet">
+			<p>@christowski you online?</p>
+		</div>
+	</div>
+</body>
+</html>
