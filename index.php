5a2076afd1cc9cd0e287ea642a06ff6f9ced9743
Lukas Oppermann <oppermann.lukas@googlemail.com>
2012-03-17 18:01:03 +1100

    adding link to tweet

--- index.php
+++ index.php
@@ -85,8 +85,12 @@
 	
 	function getLatestTweet($t_json, $twitterid)
 	{
-		$json = file_get_contents($t_json);
-		$tweets = json_decode($json, TRUE);
+		$curl = curl_init();
+		curl_setopt( $curl, CURLOPT_URL, $t_json );
+		curl_setopt( $curl, CURLOPT_RETURNTRANSFER, 1 );
+		$result = curl_exec( $curl );
+		curl_close( $curl );
+		$tweets = json_decode($result, TRUE);
 		$tweets = $tweets['results'];
 		
 		$c = 0;
